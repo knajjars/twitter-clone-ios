d0da486d97577eee8d6174a6205f48e90fe1eb62
Khalil Najjar <knajjars@gmail.com>
2020-05-13 19:30:42 -0600

    profile header

--- TwitterClone/Controller/ProfileController.swift
+++ TwitterClone/Controller/ProfileController.swift
@@ -11,17 +11,68 @@ import UIKit
 class ProfileController: UICollectionViewController {
     
     //MARK: - Properties
+    private let tweetIdentifier = "TWEET_CELL_IDENTIFIER"
+    private let headerIdentifier = "HEADER_IDENTIFIER"
     
     //MARK: - Lifecycle
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.backgroundColor = .red
+        configureUI()
     }
     
     //MARK: - Selectors
     
     //MARK: - Helpers
     
+    func configureUI() {
+        collectionView.backgroundColor = .twitterBlack
+        collectionView.contentInsetAdjustmentBehavior = .never
+        collectionView.register(TweetCell.self, forCellWithReuseIdentifier: tweetIdentifier)
+        collectionView.register(ProfileHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+    }
+    
     //MARK: - API
+    
 }
+
+//MARK: - CollectionViewController
+
+extension ProfileController {
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
+        
+        return header
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: tweetIdentifier, for: indexPath) as! TweetCell
+        
+        return cell
+    }
+}
+
+
+//MARK: - UICollectionViewDelegateFlowLayout
+extension ProfileController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 300)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 150)
+    }
+}
+
---
+++ TwitterClone/View/ProfileHeader.swift
@@ -0,0 +1,122 @@
+//
+//  ProfileHeader.swift
+//  TwitterClone
+//
+//  Created by Khalil Najjar on 5/4/20.
+//  Copyright © 2020 Khalil Najjar. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeader: UICollectionReusableView {
+    
+    //MARK: - Properties
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        
+        view.addSubview(backButton)
+        view.backgroundColor = .twitterBlue
+        backButton.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 42, paddingLeft: 16)
+        backButton.setDimensions(width: 30, height: 30)
+        
+        return view
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton()
+        
+        button.setImage(#imageLiteral(resourceName: "baseline_arrow_back_white_24dp").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var profileImage: UIImageView = {
+        let iv = UIImageView()
+        
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.backgroundColor = .twitterWhite
+        iv.layer.borderWidth = 4
+        iv.layer.borderColor = UIColor.twitterBlack.cgColor
+        
+        return iv
+    }()
+    
+    private lazy var followButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        button.setTitle("Loading...", for: .normal)
+        button.layer.borderColor = UIColor.twitterWhite.cgColor
+        button.layer.borderWidth = 1.25
+        button.setTitleColor(.twitterWhite, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.addTarget(self, action: #selector(handleFollowClick), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private let fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "My Name Test"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@myUserName"
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.numberOfLines = 3
+        label.text = "This is a label with more than one line, only for test purposes"
+        return label
+    }()
+    
+    private let bioLabel: UILabel = {
+           let label = UILabel()
+           label.font = UIFont.systemFont(ofSize: 16)
+           label.textColor = .lightGray
+           return label
+       }()
+    
+    //MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, height: 108)
+        
+        addSubview(profileImage)
+        profileImage.anchor(top: containerView.bottomAnchor, left: leftAnchor, paddingTop: -24, paddingLeft: 8)
+        profileImage.setDimensions(width: 80, height: 80)
+        profileImage.layer.cornerRadius = 80 / 2
+        
+        addSubview(followButton)
+        followButton.anchor(top: containerView.bottomAnchor, right: rightAnchor, paddingTop: 12, paddingRight: 12)
+        followButton.setDimensions(width: 100, height: 36)
+        followButton.layer.cornerRadius = 36 / 2
+        
+        let userDetailsStack = UIStackView(arrangedSubviews: [fullNameLabel,
+                                                              userNameLabel,
+                                                              bioLabel])
+        userDetailsStack.axis = .vertical
+        userDetailsStack.distribution = .fillProportionally
+        userDetailsStack.spacing = 4
+        addSubview(userDetailsStack)
+        userDetailsStack.anchor(top: profileImage.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 12, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Selectores
+    @objc func handleDismiss() {
+        print("DEBUG: dismiss clicked")
+    }
+    
+    @objc func handleFollowClick() {
+        print("DEBUG: follow clicked")
+    }
+    
+}
